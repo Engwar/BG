9eb5dd1c760db44cd3fd5e3da1e957db04903dc6
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-11-12 22:05:42 +0300

    Initial Commit

---
+++ BG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BG
+//
+//  Created by Igor Shelginskiy on 11/12/18.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
