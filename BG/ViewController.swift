758f46264fdcb31228196ea05047f4bc666e2dc1
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-11-19 01:34:10 +0300

    text field-ы думаю неудачная мысль для ячеек

--- BG/ViewController.swift
+++ BG/ViewController.swift
@@ -9,11 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var scrollView: UIScrollView!
+    
 
 
 }
