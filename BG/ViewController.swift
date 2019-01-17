775f81281d7ef3f812b274f90b2ab2ebec0802b0
Igor Shelginskiy <your@Email.com>
2019-01-17 17:45:18 +0300

    в симуляторе не видно поле с текст вью

--- BG/ViewController.swift
+++ BG/ViewController.swift
@@ -2,13 +2,22 @@
 //  ViewController.swift
 //  BG
 //
-//  Created by Igor Shelginskiy on 11/12/18.
-//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//  Created by Igor Shelginskiy on 1/17/19.
+//  Copyright © 2019 Igor Shelginskiy. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
